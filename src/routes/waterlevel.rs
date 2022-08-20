37b9ac3bd2757d0923ce142a8a5c0f53b658d493
Grant Lemons <grant0lemons@gmail.com>
2022-08-20 14:32:21 -0500

    attempt to implement get_on_date

--- src/routes/waterlevel.rs
+++ src/routes/waterlevel.rs
@@ -1,63 +1,74 @@
 use chrono::NaiveDate;
-
 use rocket::{http::Status, log::private::log, log::private::Level, serde::json::Json};
 
 use crate::diesel::prelude::*;
 use crate::models::WaterLevel;
-use crate::schema::water_levels::columns;
-use crate::schema::water_levels::table;
+use crate::schema::water_levels::{dsl, table};
 
-#[get("/")]
+#[get("/", format = "json")]
 pub fn get_all() -> Result<Json<Vec<WaterLevel>>, Status> {
     use crate::schema::water_levels::table;
     crate::get_all::<table, WaterLevel>(table)
 }
 
 /// Gets all data recorded on a certain date
-#[get("/date/<date>")]
-pub fn get_on_date(date: &str) {
+#[get("/date/<date>", format = "json")]
+pub fn get_on_date(date: &str) -> Result<Json<WaterLevel>, Status> {
     // Gives different responses depending on the validity of the passed date
-    let _date = match NaiveDate::parse_from_str(&date, "%Y-%m-%d") {
+    match NaiveDate::parse_from_str(&date, "%Y-%m-%d") {
         Ok(date) => {
             log!(Level::Info, "inputted date is {}", &date);
-            Ok(Json(format!("response: {}", &date)))
+
+            let connection = crate::establish_connection();
+
+            //TODO: Restrict to entries that match day
+            match table
+                .load::<WaterLevel>(&connection)
+            {
+                Ok(mut v) => Ok(rocket::serde::json::Json(v.remove(0))),
+                Err(_) => {
+                    rocket::log::private::log!(
+                        rocket::log::private::Level::Error,
+                        "Unable to get records!"
+                    );
+                    Err(rocket::http::Status::InternalServerError)
+                }
+            }
         }
         Err(e) => {
-            log!(Level::Error, "date from parameter failed: {}", e);
-            Err(Status::BadRequest)
+            rocket::log::private::log!(
+                rocket::log::private::Level::Error,
+                "date from parameter failed: {}",
+                e
+            );
+            Err(rocket::http::Status::BadRequest)
         }
-    };
-
-    let connection = crate::establish_connection();
-    table
-        //.filter(columns::timestamp.date().eq(date))
-        .load::<WaterLevel>(&connection)
-        .expect("Error loading config");
+    }
 }
 
-#[get("/level/at/<level>")]
+#[get("/level/at/<level>", format = "json")]
 pub fn get_at_level(level: f32) {
     let connection = crate::establish_connection();
     table
-        .filter(columns::level.eq(level as f64))
+        .filter(dsl::level.eq(level as f64))
         .load::<WaterLevel>(&connection)
         .expect("Error loading config");
 }
 
-#[get("/level/above/<level>")]
+#[get("/level/above/<level>", format = "json")]
 pub fn get_above_level(level: f32) {
     let connection = crate::establish_connection();
     table
-        .filter(columns::level.gt(level as f64))
+        .filter(dsl::level.gt(level as f64))
         .load::<WaterLevel>(&connection)
         .expect("Error loading config");
 }
 
-#[get("/level/below/<level>")]
+#[get("/level/below/<level>", format = "json")]
 pub fn get_below_level(level: f32) {
     let connection = crate::establish_connection();
     table
-        .filter(columns::level.lt(level as f64))
+        .filter(dsl::level.lt(level as f64))
         .load::<WaterLevel>(&connection)
         .expect("Error loading config");
 }
