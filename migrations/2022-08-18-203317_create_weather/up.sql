0e951120af86b743c092041d3593aaf478f9e668
Grant Lemons <grant0lemons@gmail.com>
2022-08-23 16:52:47 -0500

    add new columns to weather migration

--- migrations/2022-08-18-203317_create_weather/up.sql
+++ migrations/2022-08-18-203317_create_weather/up.sql
@@ -2,5 +2,10 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE weather (
     id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4 (),
     "location" POINT NOT NULL,
-    "timestamp" TIMESTAMP NOT NULL
+    "timestamp" TIMESTAMP NOT NULL,
+    temp float8 NOT NULL,
+    temp_min float8 NOT NULL,
+    temp_max float8 NOT NULL,
+    pressure float8 NOT NULL,
+    humidity smallint NOT NULL
 );
