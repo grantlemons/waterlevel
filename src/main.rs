9d2c36153b27e9ab6f7430fbe1ca4a1206cf3120
Grant Lemons <grant0lemons@gmail.com>
2022-08-28 17:34:21 -0500

    add doc comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+//! API used to insert and get waterlevel values into/from a postgres database as well as provide analytics and additional info based on an external weather API
+//! Provides webhook support with the [`webhooks`](crate::routes::waterlevel) endpoint
+//! Built with the [`rocket`] framework and [`diesel`] ORM
+
 #[macro_use]
 extern crate rocket;
 #[macro_use]
@@ -6,20 +10,30 @@ extern crate diesel;
 extern crate diesel_migrations;
 extern crate dotenv;
 
+/// Route specific handlers
 pub mod routes {
+    /// Handlers for waterlevel analytics
     pub mod analytics;
+    /// Configuration Handlers
     pub mod config;
+    /// Handlers for getting and inserting water levels into the database
     pub mod waterlevel;
+    /// Handlers for webhook setup
     pub mod webhooks;
 }
+/// Extra functions used in handlers and main function
 pub mod helpers;
+/// Database model structs to change from SQL types in [`schema`](crate::schema) to rust types
 pub mod models;
+/// Database schema file used by [`diesel`]
 pub mod schema;
 
 use helpers::{get_connection, get_pool, Database};
 
+/// API routing root as constant for easy configuration
 pub const ROOT: &str = "/api/v1/";
 
+/// Runs migration files with [`diesel_migrations`]
 pub fn run_migrations(database: Option<&Database>) {
     let conn = match database {
         Some(v) => get_connection(v),
@@ -32,13 +46,15 @@ pub fn run_migrations(database: Option<&Database>) {
 }
 
 #[launch]
+/// Rocket entrypoint
+/// Configures routes and shared state and runs migrations on start
 pub fn entrypoint() -> _ {
     // Create database struct
     let database = Database(get_pool());
 
     // Run database migrations
     run_migrations(Some(&database));
-    
+
     // Create rocket routes
     rocket::build()
         .manage(database)
@@ -78,6 +94,8 @@ pub fn entrypoint() -> _ {
 }
 
 #[get("/")]
+/// Healthcheck handler
+/// Checks database connection and and returns `Healthy!`
 fn health(db: &rocket::State<Database>) -> &'static str {
     get_connection(db); // check connection to db
     "Healthy!"
