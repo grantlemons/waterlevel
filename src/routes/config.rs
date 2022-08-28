258a0b8806a0bd5b09b790dbb1071789738a5e85
Grant Lemons <grant0lemons@gmail.com>
2022-08-28 17:44:14 -0500

    add doc comments

--- src/routes/config.rs
+++ src/routes/config.rs
@@ -1,3 +1,5 @@
+//! Handlers for getting, creating, and modifying configuration key-value pairs in the database
+
 use rocket::{http::Status, serde::json::Json, State};
 
 use crate::diesel::prelude::*;
@@ -7,25 +9,42 @@ use crate::schema::config::table;
 use crate::helpers::*;
 
 #[get("/")]
+/// Gets all configuration pairs set in database
 pub async fn get_all(db: &State<Database>) -> Result<Json<Vec<Config>>, Status> {
     let connection = get_connection(db);
     get_json_vec(table.load::<Config>(&connection), None)
 }
 
 #[get("/<key>")]
+/// Gets the value of a speficic key from the database
+///
+/// # Parameters
+///
+/// `/<key>` configuration option name as string
 pub async fn get_value(key: &str, db: &State<Database>) -> Result<Json<Vec<Config>>, Status> {
     let connection = get_connection(db);
     get_json_vec(table.find(key).load::<Config>(&connection), None)
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Clone)]
-pub struct Input {
+/// Form for inputs to put and post handlers in [config](self)
+pub struct ConfigForm {
+    /// Configuration option name as string
     pub key: String,
+    /// Configuration value as string
     pub value: String,
 }
 
 #[post("/", format = "json", data = "<data>")]
-pub async fn create(data: Json<Input>, db: &State<Database>) -> Result<Json<Vec<Config>>, Status> {
+/// Create new configuration key-value pair in the database
+///
+/// # Body
+///
+/// Body data should match [`ConfigForm`]
+pub async fn create(
+    data: Json<ConfigForm>,
+    db: &State<Database>,
+) -> Result<Json<Vec<Config>>, Status> {
     let connection = get_connection(db);
     let new_config = Config {
         key: data.key.clone(),
@@ -42,9 +61,18 @@ pub async fn create(data: Json<Input>, db: &State<Database>) -> Result<Json<Vec<
 }
 
 #[put("/<key>", format = "json", data = "<data>")]
+/// Modify an existing key-value pair in the database
+///
+/// # Parameters
+///
+/// `/<key>` configuration option name as string
+///
+/// # Body
+///
+/// Body data should match [`ConfigForm`]
 pub async fn modify(
     key: &str,
-    data: Json<Input>,
+    data: Json<ConfigForm>,
     db: &State<Database>,
 ) -> Result<Json<Vec<Config>>, Status> {
     let connection = get_connection(db);
